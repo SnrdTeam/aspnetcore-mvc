7f503e14238785fce7cef41abbe9c4439569b6f8
avmorozov <morozov@adeptik.com>
2019-04-25 11:55:31 +0400

    Add server header middleware

---
+++ src/Adeptik.AspNetCore.Mvc/Middleware/ServerHeaderMiddlewareExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System.Reflection;
+
+namespace Adeptik.AspNetCore.Mvc.Middleware
+{
+    public static class ServerHeaderMiddlewareExtensions
+    {
+        /// <summary>
+        /// Extentsion method that adds middleware, which populates server http header with app token and
+        /// version info.
+        /// </summary>
+        public static IApplicationBuilder UseServerHeader(this IApplicationBuilder builder)
+        {
+            var assembly = Assembly.GetEntryAssembly();
+            var assemblyName = assembly.GetCustomAttribute<AssemblyProductAttribute>().Product.Replace(" ", "-");
+            var assemblyVersion = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
+
+            return builder.Use(async (context, next) =>
+            {
+                context.Response.Headers.Add("Server", $"{assemblyName}/{assemblyVersion}");
+                await next.Invoke();
+            });
+        }
+    }
+}
